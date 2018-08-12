baafa3e33d5eefae94597f303987cc0c4b944aa0
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-08-12 23:18:11 +1200

    Add basic test with sqlalchemy

--- web2/hello.py
+++ web2/hello.py
@@ -1,11 +1,18 @@
 import tornado.ioloop
 import tornado.web
 
+import json
+
 import sqlite3
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+
+from sqlalchemy import inspect
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
 
 
 Base = declarative_base()
@@ -29,17 +36,34 @@ class MainHandler(tornado.web.RequestHandler):
 
 class PostsHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("posts")
+        session = DBSession()
+        posts = session.query(Post).all()
+        self.write("{}".format(len(posts)))
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/posts", PostsHandler)
+        (r"/posts$", PostsHandler)
     ])
 
 if __name__ == "__main__":
-    engine = create_engine('sqlite:///sqlalchemy_example.db')
+    engine = create_engine('sqlite:///posts.db')
     Base.metadata.create_all(engine)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+
+    session = DBSession()
+
+    new_user = User(name="john", passwd="foo")
+    session.add(new_user)
+    session.commit()
+
+    new_post1 = Post(text="This is a test", user_id=new_user.id)
+    new_post2 = Post(text="This is a text", user_id=new_user.id)
+    session.add(new_post1)
+    session.add(new_post2)
+    session.commit()
+
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
