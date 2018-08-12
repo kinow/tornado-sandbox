1c037e24075ba0d22322f7664d55de69407c4dda
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2018-08-12 22:57:00 +1200

    Web2 base controllers

--- web2/hello.py
+++ web2/hello.py
@@ -1,13 +1,23 @@
 import tornado.ioloop
 import tornado.web
 
+import sqlalchemy
+import sqlite3
+
+conn = sqlite3.connect('posts.db')
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class PostsHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write("posts")
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/posts", PostsHandler)
     ])
 
 if __name__ == "__main__":
