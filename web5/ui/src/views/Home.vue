4437e96a6a0f7f809064d5e7db953c1d712b286e
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-09-05 09:40:29 +1200

    Add data and placeholder HTML elements

--- web5/ui/src/views/Home.vue
+++ web5/ui/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
+    <h2>The fruit of the day is {{ fruit }}</h2>
+    <h4>And the time is {{ time }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
-  components: {
+  data () {
+    return {
+      fruit: '',
+      time: ''
+    }
   }
 }
 </script>
